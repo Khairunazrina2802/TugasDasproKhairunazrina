e12a39a6c3cf5c45b09f656aa508c576d13bc9ed
Khairunazrina2802 <56384290+Khairunazrina2802@users.noreply.github.com>
2019-10-11 15:03:10 +0700

    Add files via upload

---
+++ src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,36 @@
+
+package javaapplication12;
+
+import java.util.Scanner;
+
+public class JavaApplication12 {
+
+    public static void main(String[] args) {
+        
+        Scanner inputUser = new Scanner(System.in);
+        
+        System.out.print("Nama:");
+        String Khairun = inputUser.nextLine();
+       
+        System.out.print("Nim:");
+        String nim = inputUser.nextLine();
+        
+        System.out.print("Password:");
+        String password = inputUser.nextLine();
+        
+        System.out.println("Email:");
+        String email = inputUser.nextLine();
+       
+        System.out.println("Universitas:"); 
+        String universitas = inputUser.nextLine();
+        
+        System.out.println("Fakultas:");
+        String fakultas = inputUser.nextLine();
+        
+        System.out.println("Kelas:");
+        String kelas = inputUser.nextLine();
+        
+        
+    }
+    
+}
